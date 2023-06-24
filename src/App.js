4c39a25cfe8f5d2f252e188aea39b4d0035d206f
vancuong11 <cuongg121101@gmail.com>
2023-06-24 13:17:19 +0700

    sử dụng State và useState trong React Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+
+import { useState } from 'react';
 
 function App() {
+    // let name = 'Valne Cường'; // string
+
+    // [variable, function] = useState()
+    let [name, setName] = useState('Văn Cường');
+    const [input, setInput] = useState('');
+
+    let number = 2023; //number
+
+    const handleOnClick = () => {
+        setName(input);
+    };
+
+    const handleOnChange = (event) => {
+        setInput(event.target.value);
+    };
+    // re-render
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Hello World!</h1>
-            </header>
-        </div>
+        <>
+            <div className="App">
+                <Nav />
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>
+                        Hello World with React {name} {number}!
+                    </h1>
+                    <input type="text" value={input} onChange={(event) => handleOnChange(event)} />
+                    <button onClick={() => handleOnClick()}> Click me!</button>
+                </header>
+            </div>
+        </>
     );
 }
 
