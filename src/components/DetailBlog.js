d94f7bc759bfc1bf8cd39fe36f5ec8da088d520a
vancuong11 <cuongg121101@gmail.com>
2023-07-14 11:18:30 +0700

    Tạo trang xem chi tiết Blogs

--- src/components/DetailBlog.js
+++ src/components/DetailBlog.js
@@ -1,19 +1,42 @@
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import './Blog.scss';
 
 function DetailBlog() {
+    const [detail, setDetail] = useState([]);
+
     // navigate = useHistory
     const navigate = useNavigate();
 
     // get value param url
     const { id } = useParams();
 
+    useEffect(() => {
+        let fetchData = async () => {
+            let res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            let data = res.data;
+            setDetail(data);
+        };
+        fetchData();
+    }, []);
+
     const handleBack = () => {
         navigate('/blog');
     };
     return (
         <>
-            <div>Detail Blog id: {id}</div>
             <span onClick={() => handleBack()}>&lt;-- Back</span>
+            <div className="blog-detail">
+                {detail && (
+                    <>
+                        <div className="title">
+                            Blog Id: {id} -- {detail.title}
+                        </div>
+                        <div className="content">{detail.body}</div>
+                    </>
+                )}
+            </div>
         </>
     );
 }
